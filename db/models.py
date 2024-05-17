cfb40f80dd7c4cf3cb86084ddc998bf9034ecae6
Maryam Shabanazadeh <shabanzadeh1712@yahoo.com>
2024-05-17 22:49:17 +0200

    change name and def function store data in database and find data

---
+++ db/models.py
@@ -0,0 +1,19 @@
+from hash import Hash
+def user_serializer(user) -> dict:
+    if "password" in user and user["password"]:
+        hashed_password = Hash.bcrypt(user["password"])
+    else:
+        hashed_password = None 
+
+    return {
+        'id': str(user["_id"]),
+        'name': user["name"],
+        'email': user["email"],
+        "password": hashed_password,
+        'creat_at':user["creat_at"]
+        
+    }
+
+
+def users_serializer(users) -> list:
+    return [user_serializer(user) for user in users]
