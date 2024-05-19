05fe84f4743f4d79b91cff1fedc72a5244910f37
Maryam Shabanazadeh <shabanzadeh1712@yahoo.com>
2024-05-19 11:24:03 +0200

    create register routes

--- routes/user.py
+++ routes/user.py
@@ -1,16 +1,28 @@
-from fastapi import APIRouter
-from models.user import User
-from schemas.user import users_serializer
+from fastapi import APIRouter,Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from schemas.user import User
+from db.models import users_serializer
 from config.db import collection
+from datetime import timedelta
 
 
 user = APIRouter(prefix="/user", tags=['user'])
 
+
 @user.post("")
+async def create_user(user: User):
+    existing_user = collection.find_one({"email": user.email})
+    if existing_user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User alerady exists")
+    _id = collection.insert_one(dict(user))
+    user = users_serializer(collection.find({"_id": _id.inserted_id}))
+    return {"status": "Ok","data": user}
+@user.post("/register")
 async def create_user(user: User):
     _id = collection.insert_one(dict(user))
     user = users_serializer(collection.find({"_id": _id.inserted_id}))
     return {"status": "Ok","data": user}
+
 
 @user.get("")
 async def get_user(user: User):
