0ba6fb178a000490ff90a766ce7511d23d3ed2db
Maryam Shabanazadeh <shabanzadeh1712@yahoo.com>
2024-05-19 14:06:25 +0200

    validator password und phone

--- schemas/user.py
+++ schemas/user.py
@@ -1,20 +1,37 @@
-from models.hash import Hash
-def user_serializer(user) -> dict:
-    if "password" in user and user["password"]:
-        hashed_password = Hash.bcrypt(user["password"])
-    else:
-        hashed_password = None 
-
-    return {
-        'id': str(user["_id"]),
-        'name': user["name"],
-        'email': user["email"],
-        "password": hashed_password,
-        'creat_at':user["creat_at"]
-        
-    }
+from pydantic import BaseModel, EmailStr, constr, Field, validator
+from typing import List, Optional, Union
+from datetime import datetime
+import re
+from fastapi import Query
 
 
-def users_serializer(users) -> list:
-    return [user_serializer(user) for user in users]
+class User(BaseModel):
+    name: Union[str, None] = Query(default=None,min_length=3, max_length=50)
+    email: EmailStr
+    phone: Union[str, None] = Query(..., regex=r"^(?:\+?49|0)(?:\d{2}\)?[ -]?\d{2}[ -]?\d{7,8}|\(?\d{3}\)?[ -]?\d{3}[ -]?\d{4})$")
+    password: Union[str,None]= Query(min_length=6, max_length=30)
+    @validator('password')
+    def password_complexity(cls, value):
+        if not re.search(r'[A-Z]', value):
+            raise ValueError('Password must contain at least one uppercase letter')
+        if not re.search(r'[a-z]', value):
+            raise ValueError('Password must contain at least one lowercase letter')
+        if not re.search(r'[0-9]', value):
+            raise ValueError('Password must contain at least one digit')
+        if not re.search(r'[@$!%*?&#]', value):
+            raise ValueError('Password must contain at least one special character')
+        return value
     
+    @validator('phone')
+    def validate_phone(cls, value):
+        if value is not None:
+            if not re.match(r"^(?:\+?49|0)(?:\d{2}\)?[ -]?\d{2}[ -]?\d{7,8}|\(?\d{3}\)?[ -]?\d{3}[ -]?\d{4})$", value):
+                raise ValueError("Invalid phone number")
+        return value
+
+class UserCreate(User):
+    pass
+
+class UserInDB(User):
+    id: str
+    created_at: datetime = Field(default_factory=datetime.utcnow)
