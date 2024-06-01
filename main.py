a84f8d7b91d914d4959a9755e43a8bea61071ecd
Maryam Shabanazadeh <shabanzadeh1712@yahoo.com>
2024-06-01 17:07:36 +0200

    added cors in main

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
-from starlette.middleware.base import BaseHTTPMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 from routes.user import user
 
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware, allow_origins=['null'],
+    allow_credentials=True, allow_methods=['*'], allow_headers=['*'])
+
+
 app.include_router(user)
 
