4c350bb9b9907dcbd72bec5ae2806e63601eebde
Dmytro Shelikhov <dimaschelichov228@gmail.com>
2024-06-23 13:19:07 +0200

    Add new functionality

--- routes/user.py
+++ routes/user.py
@@ -10,48 +10,56 @@ from middelware.auth import auth_middleware
 
 user = APIRouter(prefix="/user", tags=['user'])
 
+
 @user.post("/register")
 async def create_user(user: User):
     existing_user = collection.find_one({"email": user.email})
     if existing_user:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User already exists")
-    
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="User already exists")
+
     hashed_password = Hash.bcrypt(user.password)
     user.password = hashed_password
-    
     _id = collection.insert_one(dict(user))
     user = users_serializer(collection.find({"_id": _id.inserted_id}))
     return {"status": "Ok", "data": user}
 
+
 @user.post("/login")
 async def login_user(user: UserLogin):
     found_user = collection.find_one({"name": user.name})
-    
+
     if not found_user:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User not found")
-    
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="User not found")
+
     if Hash.verify(user.password, found_user["password"]):
-        token = jwt.encode({'sub': found_user["name"]}, "test", algorithm='HS256')
+        token = jwt.encode(
+            {'sub': found_user["name"]}, "test", algorithm='HS256')
         return {"token": token}
     else:
         raise HTTPException(status_code=400, detail="Invalid credentials")
+
 
 @user.get("/{user_id}", dependencies=[Depends(auth_middleware)])
 async def detail(user_id: str):
     try:
         user_obj_id = ObjectId(user_id)
     except Exception:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid ID format")
-    
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid ID format")
+
     user_detail = collection.find_one({"_id": user_obj_id})
     if not user_detail:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Could not find user with given ID")
-    
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="Could not find user with given ID")
+
     user_remove = remove_field_document(user_detail, ["password"])
     user_remove["id"] = str(user_remove["_id"])
     user_remove.pop("_id", None)
-    
+
     return user_remove
+
 
 @user.get("")
 async def get_user():
--- schemas/user.py
+++ schemas/user.py
@@ -1,24 +1,32 @@
 from pydantic import BaseModel, EmailStr, Field, validator
 from typing import Optional
+from fastapi import APIRouter, HTTPException, status, Depends
 import re
+
 
 class User(BaseModel):
     name: str = Field(..., min_length=3, max_length=50)
     email: EmailStr
-    phone: str = Field(..., pattern=r"^(?:\+?49|0)(?:\d{2}\)?[ -]?\d{2}[ -]?\d{7,8}|\(?\d{3}\)?[ -]?\d{3}[ -]?\d{4})$")
+    phone: str = Field(...,
+                       pattern=r"^(?:\+?49|0)(?:\d{2}\)?[ -]?\d{2}[ -]?\d{7,8}|\(?\d{3}\)?[ -]?\d{3}[ -]?\d{4})$")
     password: str = Field(..., min_length=6, max_length=30)
 
     @validator('password')
     def password_complexity(cls, value):
         if not re.search(r'[A-Z]', value):
-            raise ValueError('Password must contain at least one uppercase letter')
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST, detail='Password must contain at least one uppercase letter')
         if not re.search(r'[a-z]', value):
-            raise ValueError('Password must contain at least one lowercase letter')
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST, detail='Password must contain at least one lowercase letter')
         if not re.search(r'[0-9]', value):
-            raise ValueError('Password must contain at least one digit')
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST, detail='Password must contain at least one digit')
         if not re.search(r'[@$!%*?&#]', value):
-            raise ValueError('Password must contain at least one special character')
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST, detail='Password must contain at least one special character')
         return value
+
 
 class UserLogin(BaseModel):
     name: str
